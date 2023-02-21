490e21a01f8d99a816d76861fe44d5abfb63396a
Hyunjin Kim <117606232+icecreamp@users.noreply.github.com>
2023-02-20 23:36:47 -0500

    Fix input id for overall rating

--- HJKFeedbackA2/js/HJKglobal.js
+++ HJKFeedbackA2/js/HJKglobal.js
@@ -1,15 +1,11 @@
-function init(){
 
-}
-
-$(document).ready(function () {
-    init();
+// Show the hidden rating form in the Add form page
+function showHiddenRatingFrom(){
 
     // Variable for checkbox in add form page
     const checkbox = $("#checkRating");
     // Variable of hidden form in add form page
     const ratingForm = $(".rating-form");
-
     // Hide the form for rating default
     ratingForm.hide();
 
@@ -17,6 +13,31 @@ $(document).ready(function () {
     checkbox.click(function(){
         ratingForm.toggle(600);
     });
+}
 
+// Calculate the rating
+function calculateOverall(){
+
+    let foodQualityRating = parseInt($("#foodQualityRating").val());
+    //
+    let serviceRating = parseInt($("#serviceRating").val());
+    //
+    let valueRating = parseInt($("#valueRating").val());
+    //
+    let overallRating = parseInt((foodQualityRating + serviceRating + valueRating) * 100/15);
+    $("#overallRating").val(overallRating);
+}
+
+// Default function
+function init(){
+   $("#foodQualityRating").on("input", calculateOverall());
+   $("#serviceRating").on("input", calculateOverall());
+   $("#valueRating").on("input", calculateOverall());
+}
+
+
+// When the page is loaded
+$(document).ready(function () {
+    init();
+    showHiddenRatingFrom();
 });
-
